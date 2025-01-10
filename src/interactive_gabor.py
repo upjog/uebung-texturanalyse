a2139f45e10c1d0f6dc319e62512f951c38b130f
Ramon Butzer <ramon.butzer@student.kit.edu>
2025-01-10 15:38:51 +0100

    Delete files, update hund.png

--- src/interactive_gabor.py
+++ src/interactive_gabor.py
@@ -63,70 +63,70 @@ ax.imshow(kernel)
 ax.axis('off')
 
 # Add sliders to the right of the image
-ax_ksize = plt.axes([0.7, 0.75, 0.15, 0.03], facecolor='lightgoldenrodyellow')
+ax_ksize = plt.axes((0.7, 0.75, 0.15, 0.03), facecolor='lightgoldenrodyellow')
 slider_ksize = Slider(ax_ksize, 'Ksize', 3, 100, valinit=ksize, valstep=2)
 
-ax_sigma = plt.axes([0.7, 0.65, 0.15, 0.03], facecolor='lightgoldenrodyellow')
+ax_sigma = plt.axes((0.7, 0.65, 0.15, 0.03), facecolor='lightgoldenrodyellow')
 slider_sigma = Slider(ax_sigma, 'Sigma', 1, 10, valinit=sigma)
 
-ax_theta = plt.axes([0.7, 0.55, 0.15, 0.03], facecolor='lightgoldenrodyellow')
+ax_theta = plt.axes((0.7, 0.55, 0.15, 0.03), facecolor='lightgoldenrodyellow')
 slider_theta = Slider(ax_theta, 'Theta', 0, 2*np.pi, valinit=theta)
 
-ax_lambd = plt.axes([0.7, 0.45, 0.15, 0.03], facecolor='lightgoldenrodyellow')
+ax_lambd = plt.axes((0.7, 0.45, 0.15, 0.03), facecolor='lightgoldenrodyellow')
 slider_lambd = Slider(ax_lambd, 'Lambda', 0, 2*np.pi, valinit=lambd)
 
-ax_gamma = plt.axes([0.7, 0.35, 0.15, 0.03], facecolor='lightgoldenrodyellow')
-slider_gamma = Slider(ax_gamma, 'Gamma', 0.1, 5, valinit=gamma)
+ax_gamma = plt.axes((0.7, 0.35, 0.15, 0.03), facecolor='lightgoldenrodyellow')
+slider_gamma = Slider(ax_gamma, 'Gamma', 0.1, 1, valinit=gamma)
 
-ax_phi = plt.axes([0.7, 0.25, 0.15, 0.03], facecolor='lightgoldenrodyellow')
+ax_phi = plt.axes((0.7, 0.25, 0.15, 0.03), facecolor='lightgoldenrodyellow')
 slider_phi = Slider(ax_phi, 'Phi', 0, 2*np.pi, valinit=phi)
 
 # Add arrow buttons
-ax_left_ksize = plt.axes([0.62, 0.75, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_left_ksize = plt.axes((0.62, 0.75, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_left_ksize = Button(ax_left_ksize, '<', color='lightgoldenrodyellow')
 btn_left_ksize.on_clicked(decrease_ksize)
 
-ax_right_ksize = plt.axes([0.89, 0.75, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_right_ksize = plt.axes((0.89, 0.75, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_right_ksize = Button(ax_right_ksize, '>', color='lightgoldenrodyellow')
 btn_right_ksize.on_clicked(increase_ksize)
 
-ax_left_sigma = plt.axes([0.62, 0.65, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_left_sigma = plt.axes((0.62, 0.65, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_left_sigma = Button(ax_left_sigma, '<', color='lightgoldenrodyellow')
 btn_left_sigma.on_clicked(decrease_sigma)
 
-ax_right_sigma = plt.axes([0.89, 0.65, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_right_sigma = plt.axes((0.89, 0.65, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_right_sigma = Button(ax_right_sigma, '>', color='lightgoldenrodyellow')
 btn_right_sigma.on_clicked(increase_sigma)
 
-ax_left_theta = plt.axes([0.62, 0.55, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_left_theta = plt.axes((0.62, 0.55, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_left_theta = Button(ax_left_theta, '<', color='lightgoldenrodyellow')
 btn_left_theta.on_clicked(decrease_theta)
 
-ax_right_theta = plt.axes([0.89, 0.55, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_right_theta = plt.axes((0.89, 0.55, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_right_theta = Button(ax_right_theta, '>', color='lightgoldenrodyellow')
 btn_right_theta.on_clicked(increase_theta)
 
-ax_left_lambd = plt.axes([0.62, 0.45, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_left_lambd = plt.axes((0.62, 0.45, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_left_lambd = Button(ax_left_lambd, '<', color='lightgoldenrodyellow')
 btn_left_lambd.on_clicked(decrease_lambd)
 
-ax_right_lambd = plt.axes([0.89, 0.45, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_right_lambd = plt.axes((0.89, 0.45, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_right_lambd = Button(ax_right_lambd, '>', color='lightgoldenrodyellow')
 btn_right_lambd.on_clicked(increase_lambd)
 
-ax_left_gamma = plt.axes([0.62, 0.35, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_left_gamma = plt.axes((0.62, 0.35, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_left_gamma = Button(ax_left_gamma, '<', color='lightgoldenrodyellow')
 btn_left_gamma.on_clicked(decrease_gamma)
 
-ax_right_gamma = plt.axes([0.89, 0.35, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_right_gamma = plt.axes((0.89, 0.35, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_right_gamma = Button(ax_right_gamma, '>', color='lightgoldenrodyellow')
 btn_right_gamma.on_clicked(increase_gamma)
 
-ax_left_phi = plt.axes([0.62, 0.25, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_left_phi = plt.axes((0.62, 0.25, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_left_phi = Button(ax_left_phi, '<', color='lightgoldenrodyellow')
 btn_left_phi.on_clicked(decrease_phi)
 
-ax_right_phi = plt.axes([0.89, 0.25, 0.02, 0.03], facecolor='lightgoldenrodyellow')
+ax_right_phi = plt.axes((0.89, 0.25, 0.02, 0.03), facecolor='lightgoldenrodyellow')
 btn_right_phi = Button(ax_right_phi, '>', color='lightgoldenrodyellow')
 btn_right_phi.on_clicked(increase_phi)
 
