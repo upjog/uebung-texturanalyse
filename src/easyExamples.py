b0510d9ce4b010980cbcc23fee77a88aced6d6cf
Jan H. <74530024+TimeTrvller@users.noreply.github.com>
2025-01-11 12:03:36 +0100

    - Möglichkeit zur dynamischen Ergänzung der Wellenlängen - Code und Kommentare aufgräumt - Exportnamen angepasst

--- src/easyExamples.py
+++ src/easyExamples.py
@@ -2,15 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-struktur_frequenzen = {'combine.png': [1],
-                       'hund.png': [1],
-                       'IMAG0224.jpg': [6,12,18,24],
-                       'IMG_3156.JPG': [15,20,25,30],
-                       'poolraum_bsp.jpg': [15,20,25], #12,25,70,140
-                       'text.png':[1],
-                       'woodring.png':[7,14,21,28]}
+def rep_or_ins(arr1, arr2, maxDiff):
+    """
+    Ersetze or füge Elemente aus array1 in array2 ein,
+    abhängig von der Differenz zu dem nächsten Element
 
-index = 3
+    arr1: array mit Werten, welche eingefügt werden sollen
+    arr2: array mit Werten, welche ersetzt oder ergänzt werden
+    maxDiff: inklusive Differenz, bis zu welcher Werte ersetzt statt ergänzt werden   
+    """
+    arr_return = arr2.copy()
+
+    for num in arr1:
+        idx = np.abs(arr_return - num).argmin()
+        diff = arr2[idx] - num
+
+        if np.abs(diff) <= maxDiff: # num ist nahe an einer Potenz von 2 und ersetzt diese
+            arr_return[idx] = num
+        elif diff > 0: # num ist kleiner als der nächste Wert -> wird davor eingefügt
+            np.insert(arr_return,idx, num)
+        else: # num ist größer als der nächste Wert -> wird dahinter eingefügt
+            np.insert(arr_return,idx+1, num)
+
+    return arr_return
+
+struktur_frequenzen = {'combine.png': [4,6,8,10,30],
+                       'hund.png': [10,15,20,25,30],
+                       'IMAG0224.jpg': [4,8,12,16,30],
+                       'IMG_3156.JPG': [15,20,25,30],
+                       'poolraum_bsp.jpg': [15,20,25,300], #12,25,70,140
+                       'text.png':[3,7,10,14],
+                       'woodring.png':[9,18,27,38]} #7,14,21,28
+# 0 - 6
+index = 6
 
 image_name = list(struktur_frequenzen.keys())[index]
 image_path = './images/'+image_name
@@ -20,12 +44,16 @@ test = image_name[:-4]
 
 # Filterbank parameter
 width, height = image.shape                     # Breite und Hoehe des Bildes
-ksize = int(min(width,height)*0.1)             # 10% der kleineren Bilddimension
-# ksize = 400
-gamma = 0.5                                    # Rundheit: 0.5 = Kreis, 0/1 = Linie
+ksize = int(min(width,height)*0.1)              # Kernelgröße
+gamma = 0.5                                    # Rundheit: 1 = Kreis, 0 = Linie
 psi = 0                                        # Phasenverschiebung
 # 4 Orientierungen: 0°, 45°, 90°, 135°
 orientations = [0, np.pi/4, np.pi/2, 3*np.pi/4]
+
+wavelengths = struktur_frequenzen[image_name]
+k_sigma = 12.0                                 # Faktor für Standardabweichung
+
+######### Alternativ, automatische Festlegung von lambda 
 # Min: 4/sqrt(2) = 2*sqrt(2) 
 #       -> ein Pixel hat Hypotenuse sqrt(2), 2 zueinander Diagonale Pixel ergeben doppelte Distanz
 #       -> kürzeste mögliche Wellenlänge bei 45° 
@@ -35,15 +63,16 @@ lambd_min = 2*np.sqrt(2)
 lambd_max = np.sqrt(np.abs(width)**2 + np.abs(height)**2)
 # n: Wie viele Schritte können wir von min bis max machen, wenn wir den Exponenten von 2 erhöhen
 n = int(np.log2(lambd_max/lambd_min)) 
-wavelengths2 = 2**np.arange((n-1)) * lambd_min       # Wellenlängen
-# wavelengths = wavelengths[:2]*2               # begrenzen, da größeres lambda kein anschauliches ergebnis erzeugt
-wavelengths = struktur_frequenzen[image_name]
-# wavelengths.extend([lam for lam in wavelengths2 if lam > wavelengths[-1]])
-k_sigma = 12.0                                 # Faktor für Standardabweichung
+wavelengths2 = 2**np.arange((n-1)) * lambd_min       # Wellenlängen als Potenzen von 2
+
+############# Nicht ganz so sinnvoll, da die größeren Wellenlängen keine interpretierbaren Ergebnisse zeigen 
+# # Kombination von beiden Lsiten. Wenn die manuell festgelegte Wellenlänge nahe an einer Potenz von 
+# # 2 liegt, ersetzt diese den entsprechenden Eintrag in der automatisch generierten Liste.
+# # Sonst wird die manuelle Wellenlänge ergänzt
+# wavelengths = rep_or_ins(wavelengths,wavelengths2,3)
 
 filterbank = {}
 filtered_img_bank = []
-############# hier loop
 # loop über wellenlängen, dann loop über orientierungen
 for lambd in wavelengths:
     for theta in orientations:
@@ -61,8 +90,7 @@ for idx, filtered_img in enumerate(filtered_img_bank):
     plt.axis('off')
 
 plt.tight_layout()
-plt.savefig(f"./out/{image_name[:-4]}_response_collection_lambdaMax_{round(lambd,1)}.png",dpi=300, bbox_inches='tight',transparent=False)
-# plt.show()
+plt.savefig(f"./out/response_collection_{image_name[:-4]}_lambdaMax_{round(lambd,1)}.png",dpi=300, bbox_inches='tight',transparent=False)
 
 
 # plt.figure()
