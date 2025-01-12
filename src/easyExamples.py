a62875f6de75dd103da528a2c638413478b8f4eb
Ramon Butzer <ramon.butzer@student.kit.edu>
2025-01-12 02:56:43 +0100

    Update easyExamples.py

--- src/easyExamples.py
+++ src/easyExamples.py
@@ -2,18 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-struktur_frequenzen = {'combine.png': [1],
-                       'hund.png': [1],
-                       'IMAG0224.jpg': [6,12,18,24],
-                       'IMG_3156.JPG': [15,20,25,30],
-                       'poolraum_bsp.jpg': [15,20,25], #12,25,70,140
-                       'text.png':[1],
-                       'woodring.png':[7,14,21,28]}
+struktur_frequenzen = {'combine.png': [1, 5, 6, 8, 9, 30],
+                       'hund.png': [2,4,6,8,10,12,14,16,18,20],                    # 19
+                       'IMAG0224.jpg': [6,12,18,24,32],
+                       'IMG_3156.JPG': [30],
+                       'poolraum_bsp.jpg': [17,295], #12,25,70,140
+                       'text.png':[1,2,3,7],
+                       'woodring.png':[9,38]}
 
-index = 3
+index = 1
 
 image_name = list(struktur_frequenzen.keys())[index]
-image_path = './images/'+image_name
+image_path = '../images/'+image_name
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
 test = image_name[:-4]
@@ -22,19 +22,19 @@ test = image_name[:-4]
 width, height = image.shape                     # Breite und Hoehe des Bildes
 ksize = int(min(width,height)*0.1)             # 10% der kleineren Bilddimension
 # ksize = 400
-gamma = 0.5                                    # Rundheit: 0.5 = Kreis, 0/1 = Linie
+gamma = 1                                    # Rundheit: 0.5 = Kreis, 0/1 = Linie
 psi = 0                                        # Phasenverschiebung
 # 4 Orientierungen: 0°, 45°, 90°, 135°
 orientations = [0, np.pi/4, np.pi/2, 3*np.pi/4]
-# Min: 4/sqrt(2) = 2*sqrt(2) 
+# Min: 4/sqrt(2) = 2*sqrt(2)
 #       -> ein Pixel hat Hypotenuse sqrt(2), 2 zueinander Diagonale Pixel ergeben doppelte Distanz
-#       -> kürzeste mögliche Wellenlänge bei 45° 
+#       -> kürzeste mögliche Wellenlänge bei 45°
 lambd_min = 2*np.sqrt(2)
 # Max: längeste mögl. Wellenlänge ist die Hypotenuse der Bilddimensionen
 #       -> von einer Ecke in die gegenüberleigende
 lambd_max = np.sqrt(np.abs(width)**2 + np.abs(height)**2)
 # n: Wie viele Schritte können wir von min bis max machen, wenn wir den Exponenten von 2 erhöhen
-n = int(np.log2(lambd_max/lambd_min)) 
+n = int(np.log2(lambd_max/lambd_min))
 wavelengths2 = 2**np.arange((n-1)) * lambd_min       # Wellenlängen
 # wavelengths = wavelengths[:2]*2               # begrenzen, da größeres lambda kein anschauliches ergebnis erzeugt
 wavelengths = struktur_frequenzen[image_name]
@@ -48,6 +48,8 @@ filtered_img_bank = []
 for lambd in wavelengths:
     for theta in orientations:
         kernel = cv2.getGaborKernel((ksize,ksize), k_sigma, theta, lambd, gamma, psi)
+        plt.figure()
+        plt.imshow(kernel, cmap='grey')
         key = f"theta_{int(theta*180/np.pi)}_lambda_{round(lambd,1)}"
         filterbank[key] = kernel
         filterresponse = cv2.filter2D(image,cv2.CV_8UC3, kernel)
@@ -61,8 +63,8 @@ for idx, filtered_img in enumerate(filtered_img_bank):
     plt.axis('off')
 
 plt.tight_layout()
-plt.savefig(f"./out/{image_name[:-4]}_response_collection_lambdaMax_{round(lambd,1)}.png",dpi=300, bbox_inches='tight',transparent=False)
-# plt.show()
+plt.savefig(f"../out/{image_name[:-4]}_response_collection_lambdaMax_{round(lambd,1)}.png",dpi=300, bbox_inches='tight',transparent=False)
+plt.show()
 
 
 # plt.figure()
